10a9ec376cce2e7841557f0e422c99c9a1e4242c
nallamsrikanth2 <nallamsrikanth12>
2026-04-14 11:27:49 +0530

    db

--- backend.sh
+++ backend.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-
-VALIDATE(){
-    if [ $? -ne 0 ]
-    then
-        echo -e "$2  ... $R failure $N"
-        exit 1
-    else
-        echo -e "$2   ... $G Sucess $N"
-    fi
-
-}
-if [ $USERID -ne 0 ]
-then
-    echo -e " $R please run the inside the root user $N"
-    exit 1
-else
-    echo -e " $G you are root user $N"
-fi
+source ./common.sh
+CHECK_ROOT
 
 # echo -e " $G Enter the username $N" &>>$LOGFILE
 # read -s SQLROOTPASSWORD
