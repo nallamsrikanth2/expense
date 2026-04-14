3658f39a36b98fd6e4e4c9352a119e6130d3b34d
nallamsrikanth2 <nallamsrikanth12>
2026-04-14 10:46:29 +0530

    db

--- backend.sh
+++ backend.sh
@@ -29,16 +29,16 @@ else
     echo -e " $G you are root user $N"
 fi
 
-echo -e " $G Enter the username $N"
+echo -e " $G Enter the username $N" &>>$LOGFILE
 read -s SQLROOTPASSWORD
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y  &>>$LOGFILE
 VALIDATE $? "disable nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y  &>>$LOGFILE
 VALIDATE $? "enable nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y   &>>$LOGFILE
 VALIDATE $? "install nodejs"
 
 id expense
@@ -50,44 +50,44 @@ else
     echo -e "user already created .... $Y Skipping $N"
 fi
 
-mkdir -p /app
+mkdir -p /app  &>>$LOGFILE
 VALIDATE $? "create the app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip   &>>$LOGFILE
 VALIDATE $? "download the backend code"
 
-cd /app
+cd /app  &>>$LOGFILE
 VALIDATE $? "move to app directory"
 
-rm -rf /app/*
+rm -rf /app/*  &>>$LOGFILE
 VALIDATE $? "remove the files in app directory"
 
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip  &>>$LOGFILE
 VALIDATE $? "unzip the backend code"
 
-cd /app
+cd /app  &>>$LOGFILE
 
-npm install
+npm install  &>>$LOGFILE
 VALIDATE $? "install the libries and dependies"
 
-cp /home/ec2-user/expense/backend.service   /etc/systemd/system/backend.service
+cp /home/ec2-user/expense/backend.service   /etc/systemd/system/backend.service  &>>$LOGFILE
 
-systemctl daemon-reload
+systemctl daemon-reload   &>>$LOGFILE
 VALIDATE $? "realod the code"
 
-systemctl start backend
+systemctl start backend   &>>$LOGFILE
 VALIDATE $? "start the backend"
 
-systemctl enable backend
+systemctl enable backend   &>>$LOGFILE
 VALIDATE $? "enable the code"
 
-dnf install mysql -y
+dnf install mysql -y  &>>$LOGFILE
 VALIDATE $? "install mysql"
 
-mysql -h db.nsrikanth.online -uroot -p{$SQLROOTPASSWORD} < /app/schema/backend.sql
+mysql -h db.nsrikanth.online -uroot -p{$SQLROOTPASSWORD} < /app/schema/backend.sql  &>>$LOGFILE
 VALIDATE $? "load the mysql schema"
 
-systemctl restart backend
+systemctl restart backend   &>>$LOGFILE
 VALIDATE $? "restart backend server"
 
 
