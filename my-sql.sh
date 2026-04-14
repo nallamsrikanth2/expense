70d1cca598837284819c93e7cd9441fa6fae26d8
nallamsrikanth2 <nallamsrikanth12>
2026-04-14 11:35:43 +0530

    db

--- my-sql.sh
+++ my-sql.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+CHECK_ROOT
 
 echo -e "ENTER THE USERNAME"
 read -s SQLROOTPASSWORD
 
-VALIDATE(){
-    if [ $? -ne 0 ]
-    then
-        echo -e "$2 .... $R Failue $N"
-        exit 1
-    else
-        echo -e "$2 ..... $G Sucuess $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R please run the inside root user $N"
-    exit 1
-else
-    echo -e "$G you are root user $N"
-fi
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "installing mysql"
